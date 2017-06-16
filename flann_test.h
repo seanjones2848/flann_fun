7211a531f6b3ee6d38aa825405cb62885892ba8d
Sean JONES <sjones@e1z2r2p9.42.us.org>
2017-06-16 16:38:44 -0700

    first somethings

---
+++ flann_test.h
@@ -0,0 +1,16 @@
+#ifndef FLANN_TEST_H
+# define FLANN_TEST_H
+
+# include <stdio.h>
+#include <assert.h>
+
+typedef struct	s_data
+{
+	int			rows;
+	int			cols;
+	int			t_rows;
+	int			t_cols;
+	float		speedup;
+}				t_data;
+
+#endif
